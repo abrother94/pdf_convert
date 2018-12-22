f660248be0a70b3bb4a4094755ed8695669c4317
abrother94 <abrother94@gmail.com>
2018-12-22 16:44:51 +0800

    update

--- Test1stAttr.sh
+++ Test1stAttr.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+PARAM="$1"
 `ls ME_NAME > filelist.txt`
 
 while read filename;
 do  
     echo "get form $filename"
-    STR=`./get_section.sh "ME_NAME/$filename"`
+    STR=`./get_section.sh "ME_NAME/$filename" "$PARAM"`
     STR_LEN=${#STR}
-    if (( $STR_LEN > 45 ));then
+    if (( $STR_LEN > 50 ));then
 	echo "[ME_NAME/$filename] STR[$STR] LEN[$STR_LEN]"
+	exit
     fi
 done < filelist.txt
--- get_section.sh
+++ get_section.sh
@@ -148,22 +148,18 @@ get_me_attributes_num()
     echo "[$count] attributes"
 }
 
+
 get_first_attribute_name()
 {
     local name="$1"
-    First_Attribute=`cat "$name" | tr '\n' ' ' | sed -e 's/ \+/ /g' |  sed '0,/Attributes/ s/^.*Attributes/Attributes/' |sed -n '/Attributes /,/:/p' | cut -d ":" -f1 | sed 's/Attributes //g' | sed 's/ /_/g'` 
-    echo "$First_Attribute"
-}
-
-get_first_attribute_name_()
-{
-    local name="$1"
+    local param_num="$2"
     start_line=`cat "$name" | egrep -n -m 1  'Managed entity id:' | awk -F":" '{print $1}'`
     #echo start_line[$start_line]
     content=`tail -n +$start_line "$name"` 
     #echo content[$content]
-    First_Attribute=`echo "$content" | sed -e 's/\//_/g' | awk -F"\\\\\ {2,5}" '{print $1}' | tr '\n' " "  | sed -e 's/Attributes //g' | awk -F":" '{print $2}' | sed -e 's/^ *//g' | sed -e 's/ /_/g'`
-    echo "$First_Attribute"
+    PARAM="$2"
+    Attribute_num=`echo "$content" | sed -e 's/\//_/g' | awk -F"\\\\\ {2,5}" '{print $1}' | tr '\n' " "  | sed -e 's/Attributes //g' | awk -v PARAM="$PARAM" -F":" '{print $PARAM}' | sed -e 's/^ *//g' | sed -e 's/ /_/g'`
+    echo "$Attribute_num"
 }
 
 #collect_g988_me_content_section_line_index
@@ -171,7 +167,7 @@ get_first_attribute_name_()
 #collect_me_content_section_line_index
 #dump_all_me_1
 #get_me_attributes_num "$SECTION"
-get_first_attribute_name_ "$SECTION"
+get_first_attribute_name "$SECTION" "$2"
 
 exit
 
