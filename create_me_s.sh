f3b707a631ff5010868c955bd42dd7ea4d9ed80e
abrother94 <abrother94@gmail.com>
2019-01-09 05:21:39 +0000

    update

--- create_me_s.sh
+++ create_me_s.sh
@@ -3,7 +3,9 @@ SECTION="$1"
 OUTPUT_PATH="E_OMCI/src/S_ME"
 CID=`./get_section.sh "$SECTION" me_class`
 ME_NAME=`./get_section.sh "$SECTION" me_name`
+ATT_MODE_T_AC=`./get_section.sh "$SECTION" action` 
 ATTRIB_NUM=`./get_section.sh "$SECTION" attribute_num`
+
 
 OUTPUT_FILE_NAME="$CID"_"$ME_NAME"
 OUTPUT_FILE="$OUTPUT_PATH/$OUTPUT_FILE_NAME"
@@ -38,19 +40,36 @@ J_A_END()
 
 }
 
+BASIC_SET()
+{
+    local CLASS=$1
+    local ME_NAME="$2"
+    local ACTION="$3"
+    echo "    \"Class\"       : $CLASS ,
+    \"Id\"          : 0 ,
+    \"Name\"        : \"$ME_NAME\" ,
+    \"Action\"      : \"$ACTION\", 
+    \"Created_by\"  : \"OLT\" ,
+    \"Create_Time\" : 0x0 
+    " >> ${OUTPUT_FILE}
+}
+
 ATTRIB_SET()
 {
     local ID=$1
-    local NAME="$2"
+    local ATT_NAME="$2"
     local SIZE="$3"
     local ACCESS="$4"
+    local MAND="$5"
     J_B_START
-echo "
-\"Id\"     : $ID ,
-\"Name\"   : \"$NAME\" ,
-\"Size\"   : \"$SIZE\" ,
-\"Access\" : \"$ACCESS\" 
-" >> ${OUTPUT_FILE}
+    echo "
+    \"Id\"       : $ID ,
+    \"Name\"     : \"$ATT_NAME\" ,
+    \"Size\"     : \"$SIZE\" ,
+    \"Access\"   : \"$ACCESS\"
+    \"Value\"    : 0x0
+    \"Mandtary\" : \"$MAND\"
+    " >> ${OUTPUT_FILE}
     J_B_END
 }
 
@@ -59,9 +78,24 @@ echo "
 
 J_B_START
 
-J_A_START
-ATTRIB_SET "1" "NAMEaA" "(56)" "RW"
-J_A_END
+BASIC_SET "$CID" "$ME_NAME" "$ATT_MODE_T_AC"
+J_COMMA
 
+J_A_START
+
+for (( c=1; c <= "${ATTRIB_NUM}"; c++ ))
+do 
+    ATT_NAME_T=`./get_section.sh "$SECTION" attribute_name "$c"` 
+    ATT_MODE_T_SIZE=`./get_section.sh "$SECTION" attribute_mode "$c" "SIZE"`
+    ATT_MODE_T_RW=`./get_section.sh "$SECTION" attribute_mode "$c" "RW"`
+    ATT_MODE_T_MAND=`./get_section.sh "$SECTION" attribute_mode "$c" "SET_GET"`
+    ATTRIB_SET "$c" "$ATT_NAME_T" "$ATT_MODE_T_SIZE" "$ATT_MODE_T_RW" "$ATT_MODE_T_MAND" 
+    if (( $c != "$(($ATTRIB_NUM))" ));then
+	echo "[$c] [$(($ATTRIB_NUM))]"
+	J_COMMA
+    fi
+done
+
+J_A_END
 J_B_END
 
