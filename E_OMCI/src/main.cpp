34b73aa8661ec252dc13cef3a421ecf9feead8c3
abrother94 <abrother94@gmail.com>
2019-01-09 12:50:40 +0000

    update

--- E_OMCI/src/main.cpp
+++ E_OMCI/src/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     std::string m_config_file_path = "266_GEM_interworking_termination_point";
 
     json::Value S_OMCI(json::Value::Type::OBJECT);
-//    Json::Reader reader;
+    Json::Reader reader;
 
     std::cout << "Hello World!" << std::endl;
     return 0;
